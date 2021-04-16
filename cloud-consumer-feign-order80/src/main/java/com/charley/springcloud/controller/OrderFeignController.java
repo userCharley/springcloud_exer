e8063a94c18e98acbdd9611b075fb791e4a55e4d
charley <charley99@163.com>
2021-04-16 22:22:42 +0800

    OpenFeign服务调用

---
+++ cloud-consumer-feign-order80/src/main/java/com/charley/springcloud/controller/OrderFeignController.java
@@ -0,0 +1,22 @@
+package com.charley.springcloud.controller;
+
+import com.charley.springboot.entities.CommonResult;
+import com.charley.springboot.entities.Payment;
+import com.charley.springcloud.service.PaymentFeignService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class OrderFeignController {
+
+    @Resource
+    private PaymentFeignService paymentFeignService;
+
+    @GetMapping("/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
+        return paymentFeignService.getPaymentById(id);
+    }
+}
---
+++ cloud-consumer-feign-order80/src/main/java/com/charley/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,16 @@
+package com.charley.springcloud.service;
+
+import com.charley.springboot.entities.CommonResult;
+import com.charley.springboot.entities.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE")
+public interface PaymentFeignService {
+
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
+}

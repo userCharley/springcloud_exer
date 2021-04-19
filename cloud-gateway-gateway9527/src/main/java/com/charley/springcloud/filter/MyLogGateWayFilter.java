c262d73bde26dd60f1c2ee3f021d81d0d333bf3d
charley <charley99@163.com>
2021-04-19 15:30:40 +0800

    GateWay网关

---
+++ cloud-gateway-gateway9527/src/main/java/com/charley/springcloud/filter/MyLogGateWayFilter.java
@@ -0,0 +1,34 @@
+package com.charley.springcloud.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
+/*
+自定义过滤器
+*/
+@Component
+public class MyLogGateWayFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        System.out.println("come in MyLogGateWayFilter:" + new Date());
+        String uName = exchange.getRequest().getQueryParams().getFirst("uName");
+        if (uName == null){
+            System.out.println("用户名为null，非法用户，o(╥﹏╥)o");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
---
+++ cloud-gateway-gateway9527/src/test/java/TimeTest.java
@@ -0,0 +1,10 @@
+import java.time.ZonedDateTime;
+
+public class TimeTest {
+
+    public static void main(String[] args) {
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now);
+        //2021-04-19T12:59:17.435+08:00[Asia/Shanghai]
+    }
+}
